e7816815ba4d3427a238911704428aab0c0ff463
Ryan <zjkung1123@fugle.tw>
2021-09-28 15:23:51 +0800

    init

---
+++ src/RxNetWebsocketClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reactive.Linq;
+using WebSocketSharp;
+
+string wsPath = "ws://127.0.0.1:6666/echo";
+using WebSocket webSocket = new(wsPath);
+
+//-------------------------------------------------------
+// Create an observable by wrapping ws.OnMessage
+//-------------------------------------------------------
+var globalEventStream = Observable
+    .Using(
+        () => webSocket,
+        ws => Observable.FromEventPattern<EventHandler<MessageEventArgs>, MessageEventArgs>
+        (
+            addHandler => ws.OnMessage += addHandler,
+            removeHandler => ws.OnMessage -= removeHandler
+        ));
+//---------------------------------------------------------
+// Subscribe to globalEventStream
+//---------------------------------------------------------
+IDisposable subscription = globalEventStream.Subscribe(ep =>
+{
+    Console.WriteLine("Event Received");
+    Console.WriteLine(ep.EventArgs.Data);
+});
+
+webSocket.Connect();
+Console.WriteLine("Connected!");
+webSocket.Send("Hello");
+Console.WriteLine("Sended!");
+Console.ReadKey();
+subscription.Dispose();
+webSocket.Close();
---
+++ src/WebsocketServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+var wssv = new WebSocketServer("ws://127.0.0.1:6666");
+wssv.AddWebSocketService<Echo>("/echo");
+wssv.Start();
+Console.WriteLine("Server On");
+Console.ReadKey(true);
+wssv.Stop();
+
+
+public class Echo : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Send(e.Data);
+    }
+}

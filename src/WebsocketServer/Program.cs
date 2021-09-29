2decf64caf808443227bf7c5d858e9e5f3cb2826
Ryan <zjkung1123@fugle.tw>
2021-09-29 13:47:08 +0800

    1.add server send msg by readline

--- src/WebsocketServer/Program.cs
+++ src/WebsocketServer/Program.cs
@@ -1,18 +1,39 @@
-﻿using WebSocketSharp;
+﻿using System;
+using System.Text;
+using WebSocketSharp;
 using WebSocketSharp.Server;
 
-var wssv = new WebSocketServer("ws://127.0.0.1:6666");
-wssv.AddWebSocketService<Echo>("/echo");
-wssv.Start();
+var socketServer = new WebSocketServer("ws://127.0.0.1:6666");
+socketServer.AddWebSocketService<Echo>("/echo");
+socketServer.Start();
 Console.WriteLine("Server On");
-Console.ReadKey(true);
-wssv.Stop();
+var tokenSource = new CancellationTokenSource();
+var token = tokenSource.Token;
+while (!token.IsCancellationRequested)
+{
+    //check client connected
+    if (socketServer.WebSocketServices.TryGetServiceHost("/echo", out WebSocketServiceHost host) && host.Sessions.Count > 0)
+    {
+        Stream inputStream = Console.OpenStandardInput();
+        Memory<byte> bytes = new byte[100];
+        await inputStream.ReadAsync(bytes);
+        string s = Encoding.UTF8.GetString(bytes.Span);
+        host.Sessions.BroadcastAsync(s, () => { });
+    }
+    await Task.Delay(1000);
+}
+socketServer.Stop();
 
 
 public class Echo : WebSocketBehavior
 {
+    protected override void OnOpen()
+    {
+        Console.WriteLine("Open");
+    }
     protected override void OnMessage(MessageEventArgs e)
     {
         Send(e.Data);
     }
+
 }
